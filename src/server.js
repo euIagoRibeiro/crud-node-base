c7efd399948f82df323bd4716302e61cfd7a6e6e
euIagoRibeiro <iagoribeirospider2007dpr@gmail.com>
2026-02-01 18:56:58 -0300

    feat: setup basic express server structure

--- src/server.js
+++ src/server.js
@@ -1,1 +1,23 @@
-console.log("Projeto configurado com sucesso!");
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const usuarios = [];
+
+app.get('/', (req, res) => {
+    res.send({
+        message: 'API rodando com sucesso!',
+        docs: 'Acesse /usuarios para ver a lista'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`----------------------------------------------`);
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+    console.log(`----------------------------------------------`);
+})
